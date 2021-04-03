9717afac5f4231b7ecef0388bbdee1567860c75e
fiatjaf <fiatjaf@alhur.es>
2021-04-03 19:38:17 -0300

    sign with libsecp256k1.

---
+++ secp256k1.v
@@ -0,0 +1,36 @@
+#flag -l secp256k1
+
+#include "secp256k1.h"
+#include "secp256k1_recovery.h"
+
+fn C.secp256k1_context_create(u32) &C.secp256k1_context
+fn C.secp256k1_context_destroy(&C.secp256k1_context)
+fn C.secp256k1_ecdsa_sign_recoverable(&C.secp256k1_context, &C.secp256k1_ecdsa_recoverable_signature, charptr, charptr, voidptr, voidptr) int
+fn C.secp256k1_ecdsa_recoverable_signature_serialize_compact(&C.secp256k1_context, byteptr, &int, &C.secp256k1_ecdsa_recoverable_signature) int
+
+fn sign(sighash []byte, key [32]byte) ?[]byte {
+	unsafe {
+		ctx := C.secp256k1_context_create(C.SECP256K1_CONTEXT_SIGN)
+		defer {
+			C.secp256k1_context_destroy(ctx)
+		}
+
+		rawsig := C.secp256k1_ecdsa_recoverable_signature([]C.byte{cap: 65})
+
+		dump(sighash)
+		dump(key)
+		if 0 == C.secp256k1_ecdsa_sign_recoverable(ctx, &rawsig, &sighash[0], &key[0],
+			voidptr(0), voidptr(0)) {
+			return error('failed to sign')
+		}
+
+		mut serialized := malloc(64) // []C.byte{len: 64}
+		mut recid := 0
+		if 0 == C.secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, &serialized,
+			&recid, &rawsig) {
+			return error('failed to serialize signature')
+		}
+
+		return serialized.vbytes(64)
+	}
+}
